c065d60fa06763d3a3fbef1b3b7e0b1ac4cf6b09
saiboT-Damaske <saibot01141@gmail.com>
2023-11-01 01:15:31 +0100

    challenge completed fully

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
 from flask import Flask, render_template, request
+import requests
+import smtplib
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def homepage():
-    return render_template("index.html")
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
 
 
-@app.route("/login", methods=["POST"])
-def receive_data():
-    name = request.form["user"]
-    password = request.form["pw"]
-    return f"<h1> Name: {name}, Password: {password}</h1>"
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        # 4. Send the letter generated in step 3 to that person's email address.
+        my_mail = "YOUR EMAIL"
+        password = "APP PASSWORD"
+        message = (f'name {data["name"]}\n'
+                   f'email {data["email"]}\n'
+                   f'phonenumber {data["phone"]}\n'
+                   f'message {data["message"]}')
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_mail, password=password)
+            connection.sendmail(from_addr=my_mail, to_addrs=my_mail,
+                                msg=message)
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
